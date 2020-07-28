a7120817cb48f34a2a2a758a177402992b8b8eb4
bernatzawiasa <36383460+bernatzawiasa@users.noreply.github.com>
2020-07-28 16:26:10 +0200

    responsive

--- src/app/views.cljs
+++ src/app/views.cljs
@@ -1,6 +1,13 @@
 (ns app.views
   (:require [reagent.core :as reagent :refer [atom]]))
 
+(def lang (reagent/atom "en"))
+
+(defn szoveg [angol magyar]
+  (case @lang
+    "en" angol
+    "hu" magyar))
+
 (defn app []
   [:main
    [:container
@@ -10,21 +17,30 @@
     ;   [:p "A novel species-specific machine vision technique to detect soil microarthropods."]]
     ;  [:img {:src "/images/futtató2.jpeg"}]]
     [:div.col
+    [:select {:on-change #(reset! lang (.. % -target -value))}
+      [:option (merge {:value "en" } (szoveg {:selected "selected"} nil)) "English"]
+      [:option (merge {:value "hu"} (szoveg nil {:selected "selected"})) "Magyar"]]
      [:div.alternative-title
       [:h1 "EDAPHOLOG 2.0"]
-      [:p "A novel species-specific machine vision technique to detect soil microarthropods."]]
-     [:h6 "Images taken during the project"]]
+      [:p (szoveg "A novel species-specific machine vision technique to detect soil microarthropods."
+                  "Egy korszerű, gépi látáson alapuló, fajspecifikus technika a talajlakó mikroarthropodák felismerésére.")]]
+     [:h6 (szoveg "Photo taken by EDAPHOLOG"
+                  "A fotók az EDAPHOLOG-al készültek")]]
     [:div.content
      [:div.problem
       [:div.headline
-       [:h2 "Problem & Our Solution"]]
-      [:p "Insect populations changes enormously in space and time, there is no appropriate method to be used in the applied or commercialised fields of pest management, ecology, toxicology and epidemiology. The only solution is automatic detection."]
+       [:h2 (szoveg "Problem & Our Solution"
+                    "Probléma & Megoldásunk")]]
+      [:p (szoveg "Insect populations changes enormously in space and time, there is no appropriate method to be used in the applied or commercialised fields of pest management, ecology, toxicology and epidemiology. The only solution is automatic detection."
+                  "A rovarpopulációk óriási változásokat mutatnak térben és időben, vizsgálatukra nincs megfelelő módszer a kártevőírtás, ökológia, toxikológia és a járványtan területein. Az egyetlen megoldás az automatikus felismerés.")]
       [:img {:src "/images/ss1.png"}]
       [:img {:src "/images/ss2.png"}]
-      [:p " Our Heteromurus - Folsomia classification accuracy is ~94% from 550 images"]]
+      [:p (szoveg " Our Heteromurus - Folsomia classification accuracy is ~94% from 550 images"
+                  "A Heteromurus - Folsomia osztályozásunk pontossága: ~94% - 550 képből")]]
      [:div.device
       [:div.headline
-       [:h2 "The Device"]]
+       [:h2 (szoveg "The Device"
+                    "Az eszköz")]]
       [:div.imgrow-cad
        [:img {:src "/images/futtató.jpeg"}]
        [:img {:src "/images/futtató2.jpeg"}]
@@ -36,19 +52,27 @@
        [:img {:src "/images/photo1.jpeg"}]]
       [:div.grid-container
        [:div.grid-item
-        [:h3 "Easily teachable machine"]]
+        [:h3 (szoveg "Easily teachable machine"
+                     "Könnyen tanítható eszköz")]]
        [:div.grid-item
-        [:h3 "Real-time data connection"]
-        [:p "between the server, microcomputer and user interface"]]
+        [:h3 (szoveg "Real-time data connection"
+                     "Valós idejű adatkapcsolat")]
+        [:p (szoveg "between the server, microcomputer and user interface"
+                    "a szerver, a mikroszámítógép és a felhasználói felület között")]]
        [:div.grid-item
-        [:h3 "Multi-cam motion detection"]
-        [:p "with a low power microcomputer"]]
+        [:h3 (szoveg "Multi-cam motion detection"
+                     "Több kamerás mozgásérzékelés")]
+        [:p (szoveg "with a low power microcomputer"
+                    "kis teljesítményű mikroszámítógéppel")]]
        [:div.grid-item
-        [:h3 "AI accelerator"]
-        [:p "integrated circuit developed for neural network machine learning"]]]]
+        [:h3 (szoveg "AI accelerator"
+                     "AI gyorsító")]
+        [:p (szoveg "integrated circuit developed for neural network machine learning"
+                    "integrált áramkör a neurális hálózat gépi tanulásra tervezve")]]]]
      [:div.team
       [:div.headline
-       [:h2 "Team:"]]
+       [:h2 (szoveg "Team:"
+                    "Csapat:")]]
       [:div.logo-up
        [:img {:src "/images/zoolog.png"}]
        [:img {:src "/images/taki.jpeg"}]]
