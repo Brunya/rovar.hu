1cd59a8223af10a96c9e9932f215959fbfa45fbb
Brunya <zbruno.linux@gmail.com>
2020-08-28 14:02:03 +0200

    video

--- src/app/views.cljs
+++ src/app/views.cljs
@@ -3,11 +3,6 @@
 
 (def lang (reagent/atom "en"))
 
-(defn szoveg [angol magyar]
-  (case @lang
-    "en" angol
-    "hu" magyar))
-
 (defn app []
   [:main
    [:container
@@ -17,30 +12,21 @@
     ;   [:p "A novel species-specific machine vision technique to detect soil microarthropods."]]
     ;  [:img {:src "/images/futtató2.jpeg"}]]
     [:div.col
-    [:select {:on-change #(reset! lang (.. % -target -value))}
-      [:option (merge {:value "en" } (szoveg {:selected "selected"} nil)) "English"]
-      [:option (merge {:value "hu"} (szoveg nil {:selected "selected"})) "Magyar"]]
      [:div.alternative-title
       [:h1 "EDAPHOLOG 2.0"]
-      [:p (szoveg "A novel species-specific machine vision technique to detect soil microarthropods."
-                  "Egy korszerű, gépi látáson alapuló, fajspecifikus technika a talajlakó mikroarthropodák felismerésére.")]]
-     [:h6 (szoveg "Photo taken by EDAPHOLOG"
-                  "A fotók az EDAPHOLOG-al készültek")]]
+      [:p "A novel species-specific machine vision technique to detect soil microarthropods."]]
+     [:h6 "Photo taken by EDAPHOLOG"]]
     [:div.content
      [:div.problem
-      [:div.headline
-       [:h2 (szoveg "Problem & Our Solution"
-                    "Probléma & Megoldásunk")]]
-      [:p (szoveg "Insect populations changes enormously in space and time, there is no appropriate method to be used in the applied or commercialised fields of pest management, ecology, toxicology and epidemiology. The only solution is automatic detection."
-                  "A rovarpopulációk óriási változásokat mutatnak térben és időben, vizsgálatukra nincs megfelelő módszer a kártevőírtás, ökológia, toxikológia és a járványtan területein. Az egyetlen megoldás az automatikus felismerés.")]
-      [:img {:src "/images/ss1.png"}]
-      [:img {:src "/images/ss2.png"}]
-      [:p (szoveg " Our Heteromurus - Folsomia classification accuracy is ~94% from 550 images"
-                  "A Heteromurus - Folsomia osztályozásunk pontossága: ~94% - 550 képből")]]
+       [:div.headline
+        [:h2 "Problem & Our Solution"]]
+       [:div.problem-flex
+        [:video {:width "640" :height "320" :autoplay "autoplay" :loop "loop"}
+          [:source {:src "/videos/felvetel2.mp4" :type "video/mp4"}]]
+        [:p "Insect populations changes enormously in space and time, there is no appropriate method to be used in the applied or commercialised fields of pest management, ecology, toxicology and epidemiology. The only solution is automatic detection."]]]
      [:div.device
       [:div.headline
-       [:h2 (szoveg "The Device"
-                    "Az eszköz")]]
+       [:h2 "The Device"]]
       [:div.imgrow-cad
        [:img {:src "/images/futtató.jpeg"}]
        [:img {:src "/images/futtató2.jpeg"}]
@@ -52,27 +38,19 @@
        [:img {:src "/images/photo1.jpeg"}]]
       [:div.grid-container
        [:div.grid-item
-        [:h3 (szoveg "Easily teachable machine"
-                     "Könnyen tanítható eszköz")]]
+        [:h3 "Easily teachable machine"]]
        [:div.grid-item
-        [:h3 (szoveg "Real-time data connection"
-                     "Valós idejű adatkapcsolat")]
-        [:p (szoveg "between the server, microcomputer and user interface"
-                    "a szerver, a mikroszámítógép és a felhasználói felület között")]]
+        [:h3 "Real-time data connection"]
+        [:p "between the server, microcomputer and user interface"]]
        [:div.grid-item
-        [:h3 (szoveg "Multi-cam motion detection"
-                     "Több kamerás mozgásérzékelés")]
-        [:p (szoveg "with a low power microcomputer"
-                    "kis teljesítményű mikroszámítógéppel")]]
+        [:h3 "Multi-cam motion detection"]
+        [:p "with a low power microcomputer"]]
        [:div.grid-item
-        [:h3 (szoveg "AI accelerator"
-                     "AI gyorsító")]
-        [:p (szoveg "integrated circuit developed for neural network machine learning"
-                    "integrált áramkör a neurális hálózat gépi tanulásra tervezve")]]]]
+        [:h3 "AI accelerator"]
+        [:p "integrated circuit developed for neural network machine learning"]]]]
      [:div.team
       [:div.headline
-       [:h2 (szoveg "Team:"
-                    "Csapat:")]]
+       [:h2 "Team:"]]
       [:div.logo-up
        [:img {:src "/images/zoolog.png"}]
        [:img {:src "/images/taki.jpeg"}]]
